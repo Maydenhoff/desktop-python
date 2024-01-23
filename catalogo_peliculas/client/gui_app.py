23080479eba3829867681ba8ece16330045a4615
Mayra Denhoff <mayradenhoff@gmail.com>
2024-01-23 13:04:27 -0300

    diseño

--- catalogo_peliculas/client/gui_app.py
+++ catalogo_peliculas/client/gui_app.py
@@ -1,4 +1,5 @@
 import tkinter as tk 
+from tkinter import ttk
 
 def barra_menu(root):
     barra_menu = tk.Menu(root)
@@ -11,6 +12,13 @@ def barra_menu(root):
     menu_inicio.add_command(label="Eliminar Registro en DB")
     menu_inicio.add_command(label="Salir", command= root.destroy) ##Command para el evento 
     
+    barra_menu.add_cascade(label="Consultas")
+    barra_menu.add_cascade(label="Configuración")
+    barra_menu.add_cascade(label="Ayuda")
+    
+
+
+
 
 class Frame(tk.Frame):
     def __init__(self, root = None):
@@ -18,3 +26,99 @@ class Frame(tk.Frame):
         self.root = root
         self.pack()
         self.config(bg="grey")
+
+        self.campos_peliculas()
+        self.desabiliar_campos()
+
+        self.tabla_peliculas()
+ 
+        #Campos
+    def campos_peliculas(self):
+        #labels de cada campo
+        self.label_nombre = tk.Label(self, text = "Nombre: ")
+        self.label_nombre.config(font= ("Arial", 12, "bold"))
+        self.label_nombre.grid(row= 0, column= 0, padx=10, pady=10)
+
+        self.label_duracion = tk.Label(self, text = "Duracion: ")
+        self.label_duracion.config(font= ("Arial", 12, "bold"))
+        self.label_duracion.grid(row= 1, column= 0, padx=10, pady=10)
+        
+        self.label_genero = tk.Label(self, text = "Genero: ")
+        self.label_genero.config(font= ("Arial", 12, "bold"))
+        self.label_genero.grid(row= 2, column= 0, padx=10, pady=10)
+
+        #enreys de cada campo
+        self.mi_nombre = tk.StringVar()
+        self.entry_nombre = tk.Entry(self, textvariable=self.mi_nombre)
+        self.entry_nombre.config(width= 50, font= ("Arial", 12))
+        self.entry_nombre.grid(row = 0, column= 1, padx=10, pady=10, columnspan=2)
+        
+        self.mi_duracion = tk.StringVar()
+        self.entry_duracion = tk.Entry(self, textvariable=self.mi_duracion)
+        self.entry_duracion.config(width= 50, font= ("Arial", 12))
+        self.entry_duracion.grid(row = 1, column= 1, padx=10, pady=10, columnspan=2)
+        
+        self.mi_genero = tk.StringVar()
+        self.entry_genero = tk.Entry(self, textvariable=self.mi_genero)
+        self.entry_genero.config(width= 50, font= ("Arial", 12))
+        self.entry_genero.grid(row = 2, column= 1, padx=10, pady=10, columnspan=2)
+
+        #botones
+        self.boton_nuevo = tk.Button(self, text = "Nuevo", command=self.habilitar_campos)
+        self.boton_nuevo.config(width=20, font= ("Arial", 12, "bold"), bg= "#158645", fg = "#DAD5D6", cursor="hand2", activebackground="#35BD6F"  )
+        self.boton_nuevo.grid(row=3, column=0, padx=10, pady=10)
+        
+        self.boton_guardar = tk.Button(self, text = "Guardar", command=self.guardar_datos)
+        self.boton_guardar.config(width=20, font= ("Arial", 12, "bold"), bg= "#1658A2", fg = "#DAD5D6", cursor="hand2", activebackground="#3586DF"  )
+        self.boton_guardar.grid(row=3, column=1, padx=10, pady=10)
+
+        self.boton_cancelar = tk.Button(self, text = "Cancelar", command=self.desabiliar_campos)
+        self.boton_cancelar.config(width=20, font= ("Arial", 12, "bold"), bg= "#BD152E", fg = "#DAD5D6", cursor="hand2", activebackground="#E15370"  )
+        self.boton_cancelar.grid(row=3, column=2, padx=10, pady=10)
+
+        #Metodos
+
+    def habilitar_campos(self):
+            self.mi_nombre.set("")
+            self.mi_genero.set("")
+            self.mi_duracion.set("")
+            self.entry_nombre.config(state="normal")
+            self.entry_duracion.config(state="normal")
+            self.entry_genero.config(state="normal")
+            
+            self.boton_guardar.config(state="normal")
+            self.boton_cancelar.config(state="normal")
+
+    def desabiliar_campos(self):
+            self.mi_nombre.set("")
+            self.mi_genero.set("")
+            self.mi_duracion.set("")
+
+            self.entry_nombre.config(state="disabled")
+            self.entry_duracion.config(state="disabled")
+            self.entry_genero.config(state="disabled")
+            
+            self.boton_guardar.config(state="disabled")
+            self.boton_cancelar.config(state="disabled")
+    
+    def guardar_datos(self):
+         self.desabiliar_campos()
+
+
+    def tabla_peliculas(self):
+        self.tabla = ttk.Treeview(self, column=("Nombre", "Duracion", "Genero"))
+        self.tabla.grid(row=4, column=0, columnspan=4)
+
+        self.tabla.heading("#0", text="ID")
+        self.tabla.heading("#1", text="NOMBRE")
+        self.tabla.heading("#2", text="DURACION")
+        self.tabla.heading("#3", text="GENERO")
+
+        self.tabla.insert("", 0, text=1, values=("Los vengadores", "2.35", "Acción"))
+        self.boton_editar = tk.Button(self, text="Editar")
+        self.boton_editar.config(width=20, font=("Arial", 12, "bold"), fg="#DAD5D6", bg="#158645", cursor="hand2", activebackground="#35BD6F")
+        self.boton_editar.grid(row=5, column=0, padx=10, pady=10)
+
+        self.boton_eliminar = tk.Button(self, text="Eiminar")
+        self.boton_eliminar.config(width=20, font=("Arial", 12, "bold"), fg="#DAD5D6", bg="#BD152E", cursor="hand2", activebackground="#E15370")
+        self.boton_eliminar.grid(row=5, column=1, padx=10, pady=10)
